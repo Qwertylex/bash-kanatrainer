3af17f0e1c5599e8e103caf7ad30dd55905aa840
Alex <qwertylex@gmail.com>
2012-09-06 03:29:32 -0400

    Fixes.
    Removed extra "done", made the script ignore user's "choice" case and
    slightly re-worded a few things in the comments.

--- kana.sh
+++ kana.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Alex's Kana trainer alpha0.91 (fork of my [internal] pre-alpha Hiratrainer)
-#Ideas of script skeleton taken from http://tldp.org/LDP/abs/html/randomvar.html (Example 9-12)
+#Alex's Kana trainer alpha0.92 (fork of my [internal] pre-alpha Hiratrainer)
+#Ideas for script skeleton taken from http://tldp.org/LDP/abs/html/randomvar.html (See "Example 9-12")
 #License: This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/.
 
 echo -n "hira or kata?: "
@@ -9,7 +9,7 @@ read -e choice
 hiras="あ,a い,i う,u え,e お,o か,ka き,ki く,ku け,ke こ,ko さ,sa し,shi す,su せ,se そ,so た,ta ち,chi つ,tsu て,te と,to な,na に,ni ぬ,nu ね,ne の,no は,ha ひ,hi ふ,fu へ,he ほ,ho ま,ma み,mi む,mu め,me も,mo や,ya ゆ,yu よ,yo ら,ra り,ri る,ru れ,re ろ,ro わ,wa を,wo ん,n"
 katas="ア,a イ,i ウ,u エ,e オ,o カ,ka キ,ki ク,ku ケ,ke コ,ko サ,sa シ,shi ス,su セ,se ソ,so タ,ta チ,chi ツ,tsu テ,te ト,to ナ,na ニ,ni ヌ,nu ネ,ne ノ,no ハ,ha ヒ,hi フ,fu ヘ,he ホ,ho マ,ma モ,mi ム,mu メ,me モ,mo ヤ,ya ユ,yu ヨ,yo ラ,ra リ,ri ル,ru レ,re ロ,ro ワ,wa ヲ,wo ン,n"
 
-while [ $choice == hira ]; do
+while [ `echo $choice | tr [:upper:] [:lower:]` == hira ]; do
 hira=($hiras)
 num_hiras=${#hira[*]}
 hira_in=$(echo ${hira[$((RANDOM%num_hiras))]})
@@ -25,7 +25,7 @@ if [ $romaji == $answer ];
 fi
 done
 
-while [ $choice == kata ]; do
+while [ `echo $choice | tr [:upper:] [:lower:]` == kata ]; do
 kata=($katas)
 num_katas=${#kata[*]}
 kata_in=$(echo ${kata[$((RANDOM%num_katas))]})
@@ -39,5 +39,4 @@ if [ $romaji == $answer ];
         else
         echo "Sorry, it was $romaji."
 fi
-done
 done
