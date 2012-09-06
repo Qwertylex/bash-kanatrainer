7dd515f12be43fd629b30f7b3d59a403248f1e25
Alex <qwertylex@gmail.com>
2012-09-05 20:34:59 -0400

    Making it a bit more readable.
    Expanded some abreviations and pointed out the exact script of which I
    got my 'engine' from.

--- kana.sh
+++ kana.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Alex's Kana trainer alpha0.91 (fork of my [internal] pre-alpha Hiratrainer)
-#Ideas of script skeleton taken from http://tldp.org/LDP/abs/html/randomvar.html
+#Ideas of script skeleton taken from http://tldp.org/LDP/abs/html/randomvar.html (Example 9-12)
 #License: This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/.
 
 echo -n "hira or kata?: "
 read -e choice
 
 hiras="あ,a い,i う,u え,e お,o か,ka き,ki く,ku け,ke こ,ko さ,sa し,shi す,su せ,se そ,so た,ta ち,chi つ,tsu て,te と,to な,na に,ni ぬ,nu ね,ne の,no は,ha ひ,hi ふ,fu へ,he ほ,ho ま,ma み,mi む,mu め,me も,mo や,ya ゆ,yu よ,yo ら,ra り,ri る,ru れ,re ろ,ro わ,wa を,wo ん,n"
-
 katas="ア,a イ,i ウ,u エ,e オ,o カ,ka キ,ki ク,ku ケ,ke コ,ko サ,sa シ,shi ス,su セ,se ソ,so タ,ta チ,chi ツ,tsu テ,te ト,to ナ,na ニ,ni ヌ,nu ネ,ne ノ,no ハ,ha ヒ,hi フ,fu ヘ,he ホ,ho マ,ma モ,mi ム,mu メ,me モ,mo ヤ,ya ユ,yu ヨ,yo ラ,ra リ,ri ル,ru レ,re ロ,ro ワ,wa ヲ,wo ン,n"
 
 while :
@@ -19,9 +18,9 @@ num_hiras=${#hira[*]}
 hira_in=$(echo ${hira[$((RANDOM%num_hiras))]})
 echo $hira_in | cut -d , -f 1 -
 echo -n "Romaji?: "
-read -e ans
-rom=$(echo $hira_in | cut -d , -f 2 -)
-if [ $rom == $ans ];
+read -e answer
+romaji=$(echo $hira_in | cut -d , -f 2 -)
+if [ $romaji == $answer ];
 	then
 	echo "Correct!"
 	else
@@ -33,9 +32,9 @@ num_katas=${#kata[*]}
 kata_in=$(echo ${kata[$((RANDOM%num_katas))]})
 echo $kata_in | cut -d , -f 1 -
 echo -n "Romaji?: "
-read -e ans
-rom=$(echo $kata_in | cut -d , -f 2 -)
-if [ $rom == $ans ];
+read -e answer
+romaji=$(echo $kata_in | cut -d , -f 2 -)
+if [ $romaji == $answer ];
         then
         echo "Correct!"
         else
