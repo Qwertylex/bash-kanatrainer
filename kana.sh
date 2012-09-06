d4daebdd679e497e27f4fe088433c8fce8fe5ed3
Alex <qwertylex@gmail.com>
2012-09-05 21:02:50 -0400

    Fixes and new stuff.
    Fixed a bug introduced in the last commit and re-did my while loop to
    be nicer :-)

--- kana.sh
+++ kana.sh
@@ -9,10 +9,7 @@ read -e choice
 hiras="あ,a い,i う,u え,e お,o か,ka き,ki く,ku け,ke こ,ko さ,sa し,shi す,su せ,se そ,so た,ta ち,chi つ,tsu て,te と,to な,na に,ni ぬ,nu ね,ne の,no は,ha ひ,hi ふ,fu へ,he ほ,ho ま,ma み,mi む,mu め,me も,mo や,ya ゆ,yu よ,yo ら,ra り,ri る,ru れ,re ろ,ro わ,wa を,wo ん,n"
 katas="ア,a イ,i ウ,u エ,e オ,o カ,ka キ,ki ク,ku ケ,ke コ,ko サ,sa シ,shi ス,su セ,se ソ,so タ,ta チ,chi ツ,tsu テ,te ト,to ナ,na ニ,ni ヌ,nu ネ,ne ノ,no ハ,ha ヒ,hi フ,fu ヘ,he ホ,ho マ,ma モ,mi ム,mu メ,me モ,mo ヤ,ya ユ,yu ヨ,yo ラ,ra リ,ri ル,ru レ,re ロ,ro ワ,wa ヲ,wo ン,n"
 
-while :
-do
-if [ $choice == hira ];
-then
+while [ $choice == hira ]; do
 hira=($hiras)
 num_hiras=${#hira[*]}
 hira_in=$(echo ${hira[$((RANDOM%num_hiras))]})
@@ -24,9 +21,11 @@ if [ $romaji == $answer ];
 	then
 	echo "Correct!"
 	else
-	echo "Sorry, it was $rom."
+	echo "Sorry, it was $romaji."
 fi
-else
+done
+
+while [ $choice == kata ]; do
 kata=($katas)
 num_katas=${#kata[*]}
 kata_in=$(echo ${kata[$((RANDOM%num_katas))]})
@@ -38,7 +37,7 @@ if [ $romaji == $answer ];
         then
         echo "Correct!"
         else
-        echo "Sorry, it was $rom."
+        echo "Sorry, it was $romaji."
 fi
-fi
+done
 done
