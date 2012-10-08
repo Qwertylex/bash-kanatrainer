b69fdee7d7b71a65cd5aca38a4bb754394b5bf29
Alex <qwertylex@gmail.com>
2012-10-08 14:58:30 -0400

    More neater code.
    Finally fixed "expected unary operator" and removed unnecessary echos.

--- kana.sh
+++ kana.sh
@@ -6,8 +6,7 @@
 hiras="あ,a い,i う,u え,e お,o か,ka き,ki く,ku け,ke こ,ko さ,sa し,shi す,su せ,se そ,so た,ta ち,chi つ,tsu て,te と,to な,na に,ni ぬ,nu ね,ne の,no は,ha ひ,hi ふ,fu へ,he ほ,ho ま,ma み,mi む,mu め,me も,mo や,ya ゆ,yu よ,yo ら,ra り,ri る,ru れ,re ろ,ro わ,wa を,wo ん,n"
 katas="ア,a イ,i ウ,u エ,e オ,o カ,ka キ,ki ク,ku ケ,ke コ,ko サ,sa シ,shi ス,su セ,se ソ,so タ,ta チ,chi ツ,tsu テ,te ト,to ナ,na ニ,ni ヌ,nu ネ,ne ノ,no ハ,ha ヒ,hi フ,fu ヘ,he ホ,ho マ,ma モ,mi ム,mu メ,me モ,mo ヤ,ya ユ,yu ヨ,yo ラ,ra リ,ri ル,ru レ,re ロ,ro ワ,wa ヲ,wo ン,n"
 
-echo -n "hira or kata?: "
-read -e choice
+read -e -p "hira or kata: " choice
 case $choice in
 "hira")
   kana=($hiras)
@@ -28,19 +27,17 @@ kana_randomized=$(echo ${kana[$((RANDOM%kana_enumerated))]})
 r_kana=$(echo $kana_randomized | cut -d , -f 1 -)
 romaji=$(echo $kana_randomized | cut -d , -f 2 -)
 
-echo -n "$r_kana
-Romaji?: "
-read -e answer
+read -e -p "$r_kana Romaji?: " answer
 
-if [ $romaji == $answer ];
-	then
+if [ "$romaji" == "$answer" ];
+then
 	echo "Correct!"
-    let "correct += 1"
-    let "score += 1"
-	else
+	let "correct += 1"
+	let "score += 1"
+else
 	echo "Sorry, it was $romaji."
-    let "incorrect += 1"
-    let "score -= 1"
+	let "incorrect += 1"
+	let "score -= 1"
 fi
 trap "echo; echo; echo Total Score: $score; echo Correct Answers: $correct; echo Incorrect Anwsers: $incorrect;exit 0" SIGINT SIGTERM
 done
